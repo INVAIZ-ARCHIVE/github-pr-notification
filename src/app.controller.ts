3a8f862e98dcef112d66f3e4c54c4d6d74964f39
ChangjunPark <ckdwns9121@naver.com>
2023-12-14 14:58:08 +0900

    # review

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,14 +14,10 @@ export class AppController {
   async handleWebhook(@Body() body: any) {
     console.log(body);
     try {
-      if (
-        body.payload.action === 'created' &&
-        body.payload.comment &&
-        body.payload.pull_request
-      ) {
+      if (body.action === 'created' && body.comment && body.pull_request) {
+        console.log('pr review comment');
         // Pull Request 리뷰 댓글 정보 출력
         const commentUrl = body.comment.html_url;
-
         // 리뷰 댓글 내용
         const commentBody = body.comment.body;
         const prUrl = body.pull_request.html_url;
@@ -30,20 +26,18 @@ export class AppController {
         const message = `새로운 리뷰 댓글이 달렸습니다.\n리포지토리: ${body.repository.full_name}\nPR링크:${prUrl}\n내용: ${commentBody}\n작성자: ${commenterLogin}\n댓글링크: ${commentUrl}`;
         await this.appService.sendToGoogleChat(message);
       } else if (body.issue && body.comment && body.issue.pull_request) {
+        console.log('pr comment');
+
         const commentUrl = body.comment.html_url; // Pull Request의 URL
         const commentBody = body.comment.body; // 댓글 내용
         const createUser = body.comment.user.login; // 댓글 작성자
         const message = `<users/118072138291656296236> <users/112473532277761238527> <users/103424272066547777775>\n새로운 댓글이 달렸습니다.\n리포지토리: ${body.repository.full_name}\n내용: ${commentBody}\n작성자: ${createUser}\n댓글링크: ${commentUrl}`;
         await this.appService.sendToGoogleChat(message);
-      } else if (body.pull_request.state === 'open') {
+      } else if (body.pull_request && body.pull_request.state === 'open') {
+        console.log('pr created');
         const message = `<users/118072138291656296236> <users/112473532277761238527> <users/103424272066547777775>\n새로운 PR이 등록되었습니다\n리포지토리: ${body.repository.full_name}\n링크: ${body.pull_request.html_url}\n제목: #${body.pull_request.number} ${body.pull_request.title}\n설명: ${body.pull_request.description}\n요청자: ${body.pull_request.user.login}`;
         await this.appService.sendToGoogleChat(message);
       }
-      // else if (body.pull_request.state === 'closed') {
-      //   const message = `Pull Request가 closed 되었습니다.\n리포지토리: ${body.repository.name}\n링크: ${body.pull_request.html_url}\n제목 : #${body.pull_request.number} ${body.pull_request.title}\n요청자: ${body.pull_request.user.login}
-      //   `;
-      //   await this.appService.sendToGoogleChat(message);
-      // }
     } catch (e) {
       console.log(e);
       console.log('not found pr');
