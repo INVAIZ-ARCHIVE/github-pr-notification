2d0ca1fd5509fc17ae93575a88e7d45bec2a4ecd
ChangjunPark <ckdwns9121@naver.com>
2023-12-14 15:50:29 +0900

    # test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -32,7 +32,11 @@ export class AppController {
         const createUser = body.comment.user.login; // 댓글 작성자
         const message = `<users/118072138291656296236> <users/112473532277761238527> <users/103424272066547777775>\n새로운 댓글이 달렸습니다.\n리포지토리: ${body.repository.full_name}\n내용: ${commentBody}\n작성자: ${createUser}\n댓글링크: ${commentUrl}`;
         await this.appService.sendToGoogleChat(message);
-      } else if (body.pull_request && body.pull_request.state === 'open') {
+      } else if (
+        body.pull_request &&
+        body.pull_request.state === 'open' &&
+        body.action === 'opened'
+      ) {
         console.log('pr created');
         const message = `<users/118072138291656296236> <users/112473532277761238527> <users/103424272066547777775>\n새로운 PR이 등록되었습니다\n리포지토리: ${body.repository.full_name}\n링크: ${body.pull_request.html_url}\n제목: #${body.pull_request.number} ${body.pull_request.title}\n설명: ${body.pull_request.description}\n요청자: ${body.pull_request.user.login}`;
         await this.appService.sendToGoogleChat(message);
