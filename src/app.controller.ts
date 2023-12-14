728260deb07e647e44c5ab050c2864e0accf2f14
ChangjunPark <ckdwns9121@naver.com>
2023-12-14 14:26:26 +0900

    # test commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -19,11 +19,11 @@ export class AppController {
         const commentBody = body.comment.body; // 댓글 내용
         const createUser = body.comment.user.login; // 댓글 작성자
         const message = `<users/118072138291656296236> <users/118072138291656296236> <users/103424272066547777775>\n새로운 댓글이 달렸습니다.\n 
-        리포지토리: ${body.repository.name}\n댓글링크: ${commentUrl}\n내용 : #${commentBody} ${body.pull_request.title}\n작성자 : ${createUser}\n`;
+        리포지토리: ${body.repository.full_name}\n댓글링크: ${commentUrl}\n내용: #${commentBody}\n작성자: ${createUser}\n`;
         await this.appService.sendToGoogleChat(message);
       } else if (body.pull_request.state === 'open') {
         const message = `<users/118072138291656296236> <users/118072138291656296236> <users/103424272066547777775>\n새로운 PR이 등록되었습니다\n 
-        리포지토리: ${body.repository.name}\n링크: ${body.pull_request.html_url}\n제목 : #${body.pull_request.number} ${body.pull_request.title}\n설명 : ${body.pull_request.description}\n요청자: ${body.pull_request.user.login}`;
+        리포지토리: ${body.repository.full_name}\n링크: ${body.pull_request.html_url}\n제목: #${body.pull_request.number} ${body.pull_request.title}\n설명: ${body.pull_request.description}\n요청자: ${body.pull_request.user.login}`;
         await this.appService.sendToGoogleChat(message);
       }
       // else if (body.pull_request.state === 'closed') {
