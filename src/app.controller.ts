1614462a836f6873a8c0811dee4aca8b642cb040
ChangjunPark <ckdwns9121@naver.com>
2023-12-14 11:27:07 +0900

    # pr

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,7 +12,16 @@ export class AppController {
 
   @Post('notification')
   async handleWebhook(@Body() body: any) {
-    const message = `New Pull Request: ${body.pull_request.title}`;
+    //118072138291656296236
+    //112473532277761238527
+    //103424272066547777775
+    const message = `
+    <users/118072138291656296236> <users/118072138291656296236> <users/103424272066547777775>
+    새로운 PR이 등록되었습니다\n 
+    링크: ${body.pull_request.html.url}\n
+    제목 : ${body.pull_request.title}\n
+    설명 : ${body.pull_request.description}\n
+    `;
     await this.appService.sendToGoogleChat(message);
   }
 }
