7ff2ba306922f72f49aaa00e5c806b297b0478ae
ChangjunPark <ckdwns9121@naver.com>
2023-12-14 13:31:20 +0900

    # try catch and add pr number

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,16 +12,18 @@ export class AppController {
 
   @Post('notification')
   async handleWebhook(@Body() body: any) {
-    //118072138291656296236
-    //112473532277761238527
-    //103424272066547777775
-    const message = `
-    <users/118072138291656296236> <users/118072138291656296236> <users/103424272066547777775>
-    새로운 PR이 등록되었습니다\n 
-    링크: ${body.pull_request.html.url}\n
-    제목 : ${body.pull_request.title}\n
-    설명 : ${body.pull_request.description}\n
-    `;
-    await this.appService.sendToGoogleChat(message);
+    try {
+      const message = `
+      <users/118072138291656296236> <users/118072138291656296236> <users/103424272066547777775>
+      새로운 PR이 등록되었습니다\n 
+      링크: ${body.pull_request.html.url}\n
+      제목 : #${body.pull_request.number} ${body.pull_request.title}\n
+      설명 : ${body.pull_request.description}\n
+      요청자: ${body.pull_request.user.login}
+      `;
+      await this.appService.sendToGoogleChat(message);
+    } catch (e) {
+      console.log('not found pr');
+    }
   }
 }
