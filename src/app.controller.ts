a2ca413471cf9320d7d9b68df86e93db853df367
ChangjunPark <ckdwns9121@naver.com>
2023-12-14 14:00:48 +0900

    #fixed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,16 +13,25 @@ export class AppController {
   @Post('notification')
   async handleWebhook(@Body() body: any) {
     try {
-      console.log(body.pull_request);
-      const message = `
-      <users/118072138291656296236> <users/118072138291656296236> <users/103424272066547777775>
-      새로운 PR이 등록되었습니다\n 
-      링크: ${body.pull_request.html.url}\n
-      제목 : #${body.pull_request.number} ${body.pull_request.title}\n
-      설명 : ${body.pull_request.description}\n
-      요청자: ${body.pull_request.user.login}
-      `;
-      await this.appService.sendToGoogleChat(message);
+      if (body.pull_request.state === 'open') {
+        const message = `
+        <users/118072138291656296236> <users/118072138291656296236> <users/103424272066547777775>
+        새로운 PR이 등록되었습니다\n 
+        링크: ${body.pull_request.html_url}\n
+        제목 : #${body.pull_request.number} ${body.pull_request.title}\n
+        설명 : ${body.pull_request.description}\n
+        요청자: ${body.pull_request.user.login}
+        `;
+        await this.appService.sendToGoogleChat(message);
+      } else if (body.pull_request.state === 'closed') {
+        const message = `
+        Pull Requset가 closed 되었습니다.
+        링크: ${body.pull_request.html_url}\n
+        제목 : #${body.pull_request.number} ${body.pull_request.title}\n
+        요청자: ${body.pull_request.user.login}
+        `;
+        await this.appService.sendToGoogleChat(message);
+      }
     } catch (e) {
       console.log(e);
       console.log('not found pr');
