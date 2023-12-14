e3f3b68a9698dc9988fc7b16355f16100e26df74
ChangjunPark <ckdwns9121@naver.com>
2023-12-14 14:06:04 +0900

    pr 오픈

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,11 +17,12 @@ export class AppController {
         const message = `<users/118072138291656296236> <users/118072138291656296236> <users/103424272066547777775>\n새로운 PR이 등록되었습니다\n 
         리포지토리: ${body.repository.name}\n링크: ${body.pull_request.html_url}\n제목 : #${body.pull_request.number} ${body.pull_request.title}\n설명 : ${body.pull_request.description}\n요청자: ${body.pull_request.user.login}`;
         await this.appService.sendToGoogleChat(message);
-      } else if (body.pull_request.state === 'closed') {
-        const message = `Pull Request가 closed 되었습니다.\n리포지토리: ${body.repository.name}\n링크: ${body.pull_request.html_url}\n제목 : #${body.pull_request.number} ${body.pull_request.title}\n요청자: ${body.pull_request.user.login}
-        `;
-        await this.appService.sendToGoogleChat(message);
       }
+      // else if (body.pull_request.state === 'closed') {
+      //   const message = `Pull Request가 closed 되었습니다.\n리포지토리: ${body.repository.name}\n링크: ${body.pull_request.html_url}\n제목 : #${body.pull_request.number} ${body.pull_request.title}\n요청자: ${body.pull_request.user.login}
+      //   `;
+      //   await this.appService.sendToGoogleChat(message);
+      // }
     } catch (e) {
       console.log(e);
       console.log('not found pr');
