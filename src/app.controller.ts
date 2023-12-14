8df7e770f6b4b5c640ccfda8f4b548cff732ae16
ChangjunPark <ckdwns9121@naver.com>
2023-12-14 14:04:57 +0900

    # pr 알림

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,17 +15,10 @@ export class AppController {
     try {
       if (body.pull_request.state === 'open') {
         const message = `<users/118072138291656296236> <users/118072138291656296236> <users/103424272066547777775>\n새로운 PR이 등록되었습니다\n 
-        링크: ${body.pull_request.html_url}\n
-        제목 : #${body.pull_request.number} ${body.pull_request.title}\n
-        설명 : ${body.pull_request.description}\n
-        요청자: ${body.pull_request.user.login}
-        `;
+        리포지토리: ${body.repository.name}\n링크: ${body.pull_request.html_url}\n제목 : #${body.pull_request.number} ${body.pull_request.title}\n설명 : ${body.pull_request.description}\n요청자: ${body.pull_request.user.login}`;
         await this.appService.sendToGoogleChat(message);
       } else if (body.pull_request.state === 'closed') {
-        const message = `Pull Requset가 closed 되었습니다.\n
-        링크: ${body.pull_request.html_url}\n
-        제목 : #${body.pull_request.number} ${body.pull_request.title}\n
-        요청자: ${body.pull_request.user.login}
+        const message = `Pull Requset가 closed 되었습니다.\n리포지토리: ${body.repository.name}\n링크: ${body.pull_request.html_url}\n제목 : #${body.pull_request.number} ${body.pull_request.title}\n요청자: ${body.pull_request.user.login}
         `;
         await this.appService.sendToGoogleChat(message);
       }
