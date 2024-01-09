33ebe508047b800ae58bf327b1698ab2c963b557
ChangjunPark <ckdwns9121@naver.com>
2024-01-09 11:03:35 +0900

    # 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,57 +12,6 @@ export class AppController {
 
   @Post('notification')
   async handleWebhook(@Body() body: any) {
-    try {
-      if (body.action === 'created' && body.comment && body.pull_request) {
-        console.log('pr review comment');
-        // Pull Request 리뷰 댓글 정보 출력
-        const commentUrl = body.comment.html_url;
-        // 리뷰 댓글 내용
-        const commentBody = body.comment.body;
-        const prUrl = body.pull_request.html_url;
-        // 리뷰 댓글 작성자 정보
-        const commenterLogin = body.comment.user.login;
-        const message = `새로운 리뷰 댓글이 달렸습니다.\n리포지토리: ${body.repository.full_name}\nPR링크:${prUrl}\n내용: ${commentBody}\n작성자: ${commenterLogin}\n댓글링크: ${commentUrl}`;
-        await this.appService.sendToGoogleChat(message);
-      } else if (
-        body.issue &&
-        body.comment &&
-        body.issue.pull_request &&
-        body.action === 'created'
-      ) {
-        console.log('pr comment');
-        const commentUrl = body.comment.html_url; // Pull Request의 URL
-        const commentBody = body.comment.body; // 댓글 내용
-        const createUser = body.comment.user.login; // 댓글 작성자
-        const message = `새로운 코멘트가 달렸습니다.\n리포지토리: ${body.repository.full_name}\n내용: ${commentBody}\n작성자: ${createUser}\n댓글링크: ${commentUrl}`;
-        await this.appService.sendToGoogleChat(message);
-      } else if (
-        body.pull_request &&
-        body.pull_request.state === 'open' &&
-        body.action === 'opened'
-      ) {
-        console.log('pr created');
-        const message = `<users/118072138291656296236> <users/112473532277761238527> <users/103424272066547777775>\n새로운 PR이 등록되었습니다. 리뷰해주세요~ \n리포지토리: ${body.repository.full_name}\n링크: ${body.pull_request.html_url}\n제목: #${body.pull_request.number} ${body.pull_request.title}\n요청자: ${body.pull_request.user.login}`;
-        await this.appService.sendToGoogleChat(message);
-      } else if (
-        body.pull_request &&
-        body.pull_request.state === 'closed' &&
-        body.pull_request.merged === true
-      ) {
-        console.log('pr merged');
-        const message = `병합완료✔✔ \n리포지토리: ${body.repository.full_name}\n링크: ${body.pull_request.html_url}\n제목: #${body.pull_request.number} ${body.pull_request.title}\n요청자: ${body.pull_request.user.login}`;
-        await this.appService.sendToGoogleChat(message);
-      } else {
-        console.log(body.payload);
-        console.log(body.payload.action);
-        console.log(body.payload.comment);
-        console.log(body.payload.pull_request);
-      }
-    } catch (e) {
-      console.log(e);
-      console.log('------------------------------');
-      console.log(body);
-      console.log('not found pr');
-    }
+    await this.appService.handleWebhook(body);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,168 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 
+interface Comment {
+  repository_full_name: string;
+  commentUrl: string;
+  commentBody: string;
+  prUrl: string;
+  userId: string;
+}
+
+interface PullRequest {
+  repository_full_name: string;
+  prUrl: string;
+  userId: string;
+  request_number: number;
+  title: string;
+}
+
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
 
-  async sendToGoogleChat(text: string) {
+  reviewComment({
+    repository_full_name,
+    commentUrl,
+    commentBody,
+    prUrl,
+    userId,
+  }: Comment) {
+    const message = `새로운 리뷰 댓글이 달렸습니다.\n리포지토리: ${repository_full_name}\n내용: ${commentBody}\n작성자: ${userId}\n댓글링크: ${commentUrl}\nPR링크:${prUrl}`;
+    return message;
+  }
+
+  newComment({
+    repository_full_name,
+    commentUrl,
+    commentBody,
+    prUrl,
+    userId,
+  }: Comment) {
+    const message = `새로운 코멘트가 달렸습니다.\n리포지토리: ${repository_full_name}\n내용: ${commentBody}\n작성자: ${userId}\n댓글링크: ${commentUrl}\nPR링크:${prUrl}`;
+    return message;
+  }
+
+  mergeSucceeded({
+    repository_full_name,
+    prUrl,
+    request_number,
+    title,
+    userId,
+  }: PullRequest) {
+    const message = `병합완료✔✔ \n리포지토리: ${repository_full_name}\n링크: ${prUrl}\n제목: #${request_number} ${title}\n요청자: ${userId}`;
+    return message;
+  }
+
+  newPullRequest({
+    repository_full_name,
+    prUrl,
+    request_number,
+    title,
+    userId,
+  }: PullRequest) {
+    const message = `<users/118072138291656296236> <users/112473532277761238527> <users/103424272066547777775>\n새로운 PR이 등록되었습니다. 리뷰해주세요~ \n리포지토리: ${repository_full_name}\n요청자: ${userId}\n링크: ${prUrl}\n제목: #${request_number} ${title}`;
+    return message;
+  }
+
+  async handleWebhook(body: any) {
+    try {
+      // new review
+      if (body.action === 'created' && body.comment && body.pull_request) {
+        const commentUrl = body.comment.html_url;
+        // 리뷰 댓글 내용
+        const commentBody = body.comment.body;
+        const prUrl = body.pull_request.html_url;
+        // 리뷰 댓글 작성자 정보
+        const userId = body.comment.user.login;
+        const repository_full_name = body.repository.full_name;
+        const message = this.reviewComment({
+          repository_full_name,
+          commentUrl,
+          commentBody,
+          prUrl,
+          userId,
+        });
+        await this.sendToGoogleChat(message, prUrl);
+      }
+
+      // new comment
+      else if (
+        body.issue &&
+        body.comment &&
+        body.issue.pull_request &&
+        body.action === 'created'
+      ) {
+        const commentUrl = body.comment.html_url;
+        // 리뷰 댓글 내용
+        const commentBody = body.comment.body;
+        const prUrl = body.pull_request.html_url;
+        // 리뷰 댓글 작성자 정보
+        const userId = body.comment.user.login;
+        const repository_full_name = body.repository.full_name;
+
+        const message = this.newComment({
+          repository_full_name,
+          commentUrl,
+          commentBody,
+          prUrl,
+          userId,
+        });
+        await this.sendToGoogleChat(message, prUrl);
+      }
+
+      // new pr
+      else if (
+        body.pull_request &&
+        body.pull_request.state === 'open' &&
+        body.action === 'opened'
+      ) {
+        const repository_full_name = body.repository.full_name;
+        const prUrl = body.pull_request.html_url;
+        const request_number = body.pull_request.number;
+        const title = body.pull_request.title;
+        const userId = body.pull_request.user.login;
+
+        const message = this.newPullRequest({
+          repository_full_name,
+          prUrl,
+          request_number,
+          title,
+          userId,
+        });
+        await this.sendToGoogleChat(message, prUrl);
+      }
+
+      // merge
+      else if (
+        body.pull_request &&
+        body.pull_request.state === 'closed' &&
+        body.pull_request.merged === true
+      ) {
+        const repository_full_name = body.repository.full_name;
+        const prUrl = body.pull_request.html_url;
+        const request_number = body.pull_request.number;
+        const title = body.pull_request.title;
+        const userId = body.pull_request.user.login;
+        const message = this.mergeSucceeded({
+          repository_full_name,
+          prUrl,
+          request_number,
+          title,
+          userId,
+        });
+        await this.sendToGoogleChat(message, prUrl);
+      }
+    } catch (e) {
+      console.log(e);
+      console.log('------------------------------');
+      console.log('not found pr');
+    }
+  }
+
+  async sendToGoogleChat(text: string, thread: string) {
     try {
       const configs = {
         headers: {
@@ -16,8 +171,15 @@ export class AppService {
       };
 
       const url =
-        'https://chat.googleapis.com/v1/spaces/AAAAswwL7Fg/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=DBb0Nyvj8YqsdKHm2kQOqZ68NErC5_Ab45JwKJYQ6N8';
-      await axios.post(url, { text }, configs);
+        'https://chat.googleapis.com/v1/spaces/AAAAswwL7Fg/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=DBb0Nyvj8YqsdKHm2kQOqZ68NErC5_Ab45JwKJYQ6N8&messageReplyOption=REPLY_MESSAGE_FALLBACK_TO_NEW_THREAD';
+      await axios.post(
+        url,
+        {
+          text,
+          thread: thread && { threadKey: thread },
+        },
+        configs,
+      );
     } catch (e) {
       console.log('google chat webhook error');
     }
